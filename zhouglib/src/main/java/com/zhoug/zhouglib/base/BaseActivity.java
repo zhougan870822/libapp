f622bfbc3d0ef14e4eed3b7452c1a432edf506b9
zhougan <564169392@qq.com>
2019-07-30 12:19:29 +0800

    第一次项目创建完成

---
+++ zhouglib/src/main/java/com/zhoug/zhouglib/base/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.zhoug.zhouglib.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhoug.zhouglib.R;
+
+/**
+ * 基类Activity
+ *
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
---
+++ zhouglib/src/main/java/com/zhoug/zhouglib/util/StringUtil.java
@@ -0,0 +1,51 @@
+package com.zhoug.zhouglib.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * 字符串工具包
+ */
+public class StringUtil {
+    /**
+     * 全角空格，长度等于一个中文字符
+     */
+    public static final String BLANK_SPACE_2="&#8195;";
+    /**
+     * 全角空格，长度等于半个中文字符
+     */
+    public static final String BLANK_SPACE_1="&#8194;";
+    /**
+     * 不会被合并的空格，长度与常规空格相同
+     */
+    public static final String BLANK_SPACE="&#160;";
+    /**email的正则表达式*/
+    private final static Pattern emailer = Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
+
+
+    /**
+     * 断给定字符串是否空白串
+     * null、"null"、" "
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str){
+        if(str==null || "".equals(str.trim())|| "null".equals(str))
+            return true;
+        return false;
+    }
+
+    /**
+     * 判断是不是一个合法的电子邮件地址
+     * @param email
+     * @return
+     */
+    public static boolean isEmail(String email) {
+
+        return !isEmpty(email) && emailer.matcher(email).matches();
+    }
+
+
+
+
+
+}
